4d65fa95cdf325093003e822c6b8f76fa024dbfd
8hrsk <insanitykape@gmail.com>
2024-12-25 19:45:40 +0300

    forgot to uncomment

--- scripts/blockcheck.sh
+++ scripts/blockcheck.sh
@@ -7,7 +7,7 @@ SUMMARY_FILE="blockcheck_summary.txt"
 # ls /configuration/
 
 # Выполняем скрипт и записываем весь вывод в файл
-# printf '\n' '\n' '\n' '\n' '\n' '\n' '\n' | ./blockcheck.sh | tee /configuration/"$LOG_FILE"
+printf '\n' '\n' '\n' '\n' '\n' '\n' '\n' | ./blockcheck.sh | tee /configuration/"$LOG_FILE"
 
 grep -A 1000 -i "summary" /configuration/"$LOG_FILE" | grep -B 1000 -i "Please" | grep -v -i "Please" > /configuration/"$SUMMARY_FILE"
 
