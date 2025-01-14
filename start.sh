7b206945f46b39da56c079489a1064329b3d40f3
F33RNI <fernlane@proton.me>
2025-01-14 15:01:50 -0500

    chore: style + comments

--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Make sure container is stopped
 ./stop.sh
 
+# Build and start container
 docker-compose build
 docker-compose up -d
 
-# wait for the container to be ready
+# Wait for the container to be ready
 until docker exec zapret-proxy echo "Container is ready"; do
   echo "Waiting for container to start..."
   sleep 2
 done
 
+# Execute starting script
 docker exec -it zapret-proxy ./start_zapret.sh
