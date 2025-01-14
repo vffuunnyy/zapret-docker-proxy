15e62b9ec0f9fe6728ccf8c7f0bdb5bed8604410
F33RNI <fernlane@proton.me>
2025-01-14 15:46:01 -0500

    chore: shorten command

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@
 ./stop.sh
 
 # Build and start container
-docker-compose build
-docker-compose up -d
+docker-compose up --build -d
 
 # Wait for the container to be ready
 until docker exec zapret-proxy echo "Container is ready"; do
