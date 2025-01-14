91f0d533719994139b1670d5b1301f292b4514e4
F33RNI <fernlane@proton.me>
2025-01-14 18:46:57 -0500

    fix: zapret config overwriting + services start

--- scripts/enable_services.sh
+++ scripts/enable_services.sh
@@ -5,19 +5,19 @@ echo "Enabling and starting services..."
 echo "Reloading systemctl daemon..."
 systemctl daemon-reload
 
-# echo "Stopping systemd-resolved service..."
-# systemctl stop systemd-resolved
+echo "Stopping systemd-resolved service..."
+systemctl stop systemd-resolved
 
 # echo "Restarting dnsmasq.service service..."
 # systemctl restart dnsmasq.service
 
-echo "Enabling and restarting dnscrypt-proxy service..."
+echo "Enabling and restarting dnscrypt-proxy service... This might take a while"
 systemctl enable dnscrypt-proxy.service
 systemctl restart dnscrypt-proxy.service
 
-echo "Enabling and restarting systemd-resolved service..."
-systemctl enable systemd-resolved.service
-systemctl restart systemd-resolved.service
+# echo "Enabling and restarting systemd-resolved service..."
+#systemctl enable systemd-resolved.service
+#systemctl restart systemd-resolved.service
 
 echo "Restarting squid service... This might take a while"
 systemctl restart squid
--- scripts/start_zapret.sh
+++ scripts/start_zapret.sh
@@ -3,10 +3,26 @@
 # Enable and start services
 ./enable_services.sh
 
+# Copy existing config to prevent overwriting it
+if [ -f "/opt/zapret/config" ]; then
+    echo "Renaming config file into /opt/zapret/config_temp to prevent overwriting"
+    mv "/opt/zapret/config" "/opt/zapret/config_temp"
+fi
+
 # Install zapret
 echo "Installing zapret..."
-printf '%s\n' '\n' 'n' '\n' 'n' '\n' 'n' '\n' 'n' | ./install_easy.sh
+echo -ne "\n" | ./install_easy.sh
 echo "zapret installed"
 
+# Restore config
+if [ -f "/opt/zapret/config_temp" ]; then
+    echo "Restoring config file from /opt/zapret/config_temp"
+    mv "/opt/zapret/config_temp" "/opt/zapret/config"
+fi
+
+# Restart zapret
+echo "Restarting zapret service..."
+systemctl restart zapret
 echo "Done!"
+
 exit 0
