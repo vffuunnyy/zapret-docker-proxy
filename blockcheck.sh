aa3bb653e21b8f5ea04b72b1f5a870971fa325fd
F33RNI <fernlane@proton.me>
2025-01-14 20:02:08 -0500

    feat: split blockcheck for manual run + docs update

---
+++ blockcheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Make sure container is stopped
+./stop.sh
+
+# Build and start container
+docker-compose up --build -d
+
+# Wait for the container to be ready
+until docker exec zapret-proxy echo "Container is ready"; do
+  echo "Waiting for container to start..."
+  sleep 2
+done
+
+# Enable and start services
+docker exec zapret-proxy ./enable_services.sh
+
+# Run blockcheck.sh
+docker exec zapret-proxy ./blockcheck_in_container.sh
