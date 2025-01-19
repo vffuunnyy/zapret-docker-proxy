7da06097ab8f4613c8af6ff240e263a06dba1a73
F33RNI <fernlane@proton.me>
2025-01-19 14:31:40 -0500

    feat: automatically download latest releases of zapret and dnscrypt-proxy

--- quickstart.sh
+++ quickstart.sh
@@ -1,34 +1,60 @@
 #!/bin/bash
 
-# TODO: use latest versions
-ZAPRET_TAG="v69.9"
-DNSCRYPT_PROXY_TAG="2.1.7"
+# See <https://github.com/DNSCrypt/dnscrypt-proxy/releases/latest> for more info
+DNSCRYPT_PLATFORM="linux"
+DNSCRYPT_ARCH="x86_64"
 
 # Remove old zapret directory if exists
 if [ -d "./zapret" ]; then rm -Rf ./zapret; fi
 
 # Download zapret
-wget "https://github.com/bol-van/zapret/releases/download/$ZAPRET_TAG/zapret-$ZAPRET_TAG.tar.gz"
-if ! tar -xvzf "zapret-$ZAPRET_TAG.tar.gz"; then
-    tar xvzf "zapret-$ZAPRET_TAG.tar.gz"
+download_url=$(curl -s https://api.github.com/repos/bol-van/zapret/releases/latest | grep -oP '"browser_download_url": "\K.*?\.tar\.gz(?=")' | grep -v "openwrt")
+if [ -z "$download_url" ]; then
+    echo "Error: Unable to find .tar.gz asset in the latest release of zapret"
+    exit 1
+fi
+filename=$(basename "$download_url")
+echo "Downloading $download_url"
+wget "$download_url"
+if ! tar -xvzf "$filename"; then
+    tar xvzf "$filename"
 fi
 
 # Extract it
-rm "zapret-$ZAPRET_TAG.tar.gz"
-mv "./zapret-$ZAPRET_TAG" ./zapret
+rm "$filename"
+mv "./${filename%.*.*}" ./zapret
+
+# Check
+if [ ! -d "./zapret" ]; then
+    echo "Error downloading or extracting zapret binaries"
+    exit 1
+fi
 
 # Remove old dnscrypt-proxy directory if exists
 if [ -d "./dnscrypt-proxy" ]; then rm -Rf ./dnscrypt-proxy; fi
 
 # Download dnscrypt-proxy
-wget "https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/$DNSCRYPT_PROXY_TAG/dnscrypt-proxy-linux_x86_64-$DNSCRYPT_PROXY_TAG.tar.gz"
-if ! tar -xvzf "dnscrypt-proxy-linux_x86_64-$DNSCRYPT_PROXY_TAG.tar.gz"; then
-    tar xvzf "dnscrypt-proxy-linux_x86_64-$DNSCRYPT_PROXY_TAG.tar.gz"
+download_url=$(curl -s https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest | grep -oP '"browser_download_url": "\K.*?\.tar\.gz(?=")' | grep -oE ".*dnscrypt-proxy-${DNSCRYPT_PLATFORM}_${DNSCRYPT_ARCH}-.*\.tar\.gz")
+if [ -z "$download_url" ]; then
+    echo "Error: Unable to find .tar.gz asset in the latest release of dnscrypt-proxy for ${DNSCRYPT_PLATFORM} ${DNSCRYPT_ARCH}"
+    exit 1
+fi
+filename=$(basename "$download_url")
+echo "Downloading $download_url"
+wget "$download_url"
+if ! tar -xvzf "$filename"; then
+    tar xvzf "$filename"
 fi
 
 # Extract it
-rm "dnscrypt-proxy-linux_x86_64-$DNSCRYPT_PROXY_TAG.tar.gz"
-mv "./linux-x86_64" ./dnscrypt-proxy
+rm "$filename"
+mv "./${DNSCRYPT_PLATFORM}-${DNSCRYPT_ARCH}" ./dnscrypt-proxy
+
+# Check
+if [ ! -d "./dnscrypt-proxy" ]; then
+    echo "Error downloading or extracting dnscrypt-proxy binaries"
+    exit 1
+fi
 
 # Start build script
 ./start.sh
